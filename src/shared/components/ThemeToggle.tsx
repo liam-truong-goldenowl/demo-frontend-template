79ac93721280d3200de85d27ed24fa6ae130ad6c
Liam Truong <liam.truong.goldenowl@gmail.com>
2025-07-21 15:11:40 +0700

    feat: update theme toggle to use intl

--- src/shared/components/ThemeToggle.tsx
+++ src/shared/components/ThemeToggle.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useTheme } from 'next-themes';
-import { Sun, Moon } from 'lucide-react';
+import { useTranslations } from 'next-intl';
+import { Sun, Check, MoonStar } from 'lucide-react';
 
 import { Button } from '@/shared/components/ui/button';
 import {
@@ -11,28 +12,31 @@ import {
   DropdownMenuTrigger,
 } from '@/shared/components/ui/dropdown-menu';
 
+enum Theme {
+  Light = 'light',
+  Dark = 'dark',
+  System = 'system',
+}
+
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme: currentTheme = Theme.System } = useTheme();
+  const t = useTranslations('ThemeToggle');
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="[--icon-size:1.2rem]">
+        <Button variant="outline" size="icon" className="[--icon-size:1rem]">
           <Sun className="size-(--icon-size) scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute size-(--icon-size) scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+          <MoonStar className="absolute size-(--icon-size) scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">{t('trigger')}</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
+        {Object.values(Theme).map((theme) => (
+          <DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
+            {t(theme)} {currentTheme === theme && <Check />}
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
